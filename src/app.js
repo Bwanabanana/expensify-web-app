1545560d66dc2c216b8ead45f6b8e1b19450803e
Alex Davis <alexander_davis@hotmail.co.uk>
2020-08-16 17:42:01 +0100

    Styled login page

--- src/app.js
+++ src/app.js
@@ -1,20 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Amplify, { Auth } from 'aws-amplify';
 import App from './components/App';
 import 'react-dates-temp/initialize';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 import 'react-dates-temp/lib/css/_datepicker.css';
 
-const ampConfig = Amplify.configure({
-    Auth: {
-        region: 'eu-west-2',
-        userPoolId: 'eu-west-2_59mzxsHwL',
-        userPoolWebClientId: '2dmom3jhfm11466uvk7lmp91br'
-    }
-});
-
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(<App className="box-layout" />, document.getElementById('app'));
 
 
--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,31 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-// import { Auth } from 'aws-amplify';
+import Amplify from 'aws-amplify';
 import AppRouter from '../routers/AppRouter';
+import { Authenticator } from 'aws-amplify-react';
 import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
 import configureStore from '../store/configureStore';
 import { loadExpenses } from '../actions/expenses';
+
+const ampConfig = Amplify.configure({
+    Auth: {
+        region: 'eu-west-2',
+        userPoolId: 'eu-west-2_59mzxsHwL',
+        userPoolWebClientId: '2dmom3jhfm11466uvk7lmp91br'
+    }
+});
+
+const myTheme = {
+    container: {
+        'alignItems': 'center',
+        'background': `url('images/bg.jpg')`,
+        'backgroundSize': 'cover',
+        'display': 'flex',
+        'height': '100vh',
+        'justifyContent': 'center',
+        'width': '100vw'
+    }
+};
 
 const store = configureStore();
 
@@ -29,21 +50,34 @@ class App extends React.Component {
     }
 
     render() {
-
-        return (
-            <div>
-                <AmplifySignOut />
-                {this.state.loading
-                    ?
+        if (this.state.loading) {
+            return (
+                <div>
+                    <AmplifySignOut />
                     <h2>Retrieving Expenses...</h2>
-                    :
+                </div>
+            );
+        } else {
+            return (
+                <div>
+                    <AmplifySignOut />
                     <Provider store={store}>
                         <AppRouter />
                     </Provider>
-                }
-            </div>
-        )
+                </div>
+            )
+        }
     }
 }
 
-export default withAuthenticator(App, { includeGreeting: true, initialAuthState: 'signin' });
+const AppWithAuthenticator = withAuthenticator(App, false, []);
+
+export default class AppWithAuth extends React.Component {
+    render() {
+        return (
+            <Authenticator hideDefault={true} theme={myTheme}>
+                <AppWithAuthenticator />
+            </Authenticator>
+        )
+    }
+}
